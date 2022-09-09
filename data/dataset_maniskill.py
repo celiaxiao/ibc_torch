14f30cb2987784866ecf21285fb728d6c57d6f74
TangYihe <yihe1125@gmail.com>
2022-09-08 23:57:36 -0700

    [fix&stagging] 0908. pointcloud compatible

--- data/dataset_maniskill.py
+++ data/dataset_maniskill.py
@@ -70,6 +70,34 @@ class state_dataset(Dataset):
         act = self.experiences[idx]['action']
         return obs, act
 
+class pointcloud_dataset(Dataset):
+    '''
+    Dataset for maniskill2 softbody envs, pointcloud obs mode
+    Input:
+    -- observations: list of observations. Need to be pre-processed to 1d. Visual components first.
+    -- actions: list of actions. index should match observations
+    '''
+    def __init__(self, observations, actions, device=None):
+        experiences = []
+        if device is None:
+            device = torch.device('cuda')
+        for idx in range(len(observations)):
+            exp = {}
+            exp['observation'] = torch.tensor(observations[idx]).to(device)
+            exp['action'] = torch.tensor(actions[idx]).to(device)
+            experiences.append(exp)
+            # print(exp['observation'].shape)
+            # exit(0)
+        self.experiences = experiences
+
+    def __len__(self):
+        return len(self.experiences)
+
+    def __getitem__(self, idx):
+        obs = self.experiences[idx]['observation']
+        act = self.experiences[idx]['action']
+        return obs, act
+
 def save_dataset(dataset, env_name):
     print(dataset['observations'].shape, dataset['actions'].shape) # An N x dim_observation Numpy array of observations
 
@@ -92,12 +120,12 @@ def get_action_stat(dataset, env_name):
 if __name__ == '__main__':
     # Hang-v0: ?? experience pair, xyz shape (??, 1024, 3), agent shape (??, 25), act shape (??, 8)
 
-    observations = np.load("/home/yihe/ibc_torch/work_dirs/demos/excavate_obs_100traj.npy", allow_pickle=True)
-    actions = np.load("/home/yihe/ibc_torch/work_dirs/demos/excavate_act_100traj.npy")
+    observations = np.load("/home/yihe/ibc_torch/work_dirs/formal_demos/Hang-v0/pcd_observations.npy", allow_pickle=True)
+    actions = np.load("/home/yihe/ibc_torch/work_dirs/formal_demos/Hang-v0/actions.npy")
 
-    dataset = particle_dataset(observations, actions)
+    dataset = pointcloud_dataset(observations, actions)
     print(len(dataset))
-    torch.save(dataset, '/home/yihe/ibc_torch/work_dirs/demos/excavate_particles_10kPairs.pt')
+    torch.save(dataset, '/home/yihe/ibc_torch/work_dirs/formal_demos/Hang-v0/pd_joint_delta_pcd.pt')
     
 
     
