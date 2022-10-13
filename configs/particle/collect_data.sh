0c9632cc366e3005645d005ef288536b41c272bf
celiaxiao <c6xiao@ucsd.edu>
2022-10-13 12:59:18 -0700

    update config file

--- configs/particle/collect_data.sh
+++ configs/particle/collect_data.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-python3 data/policy_eval.py -- \
- --alsologtostderr \
+python data/collect_policy.py \
  --num_episodes=200 \
- --policy=particle_green_then_blue \
  --task=PARTICLE \
- --dataset_path=ibc/data/particle/2d_oracle_particle.tfrecord \
- --replicas=10  \
- --use_image_obs=False
--- configs/pushing_states/collect_data.sh
+++ configs/pushing_states/collect_data.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-python3 data/policy_eval.py -- \
- --alsologtostderr \
+python data/collect_policy.py \
  --num_episodes=200 \
- --policy=oracle_push \
  --task=PUSH \
- --dataset_path=ibc/data/block_push_states_location/oracle_push.tfrecord \
- --replicas=10  \
- --use_image_obs=False
