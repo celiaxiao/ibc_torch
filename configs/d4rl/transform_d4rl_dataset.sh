5211a4c9056bee764b298bb1ca5202d8e783afb5
celiaxiao <c6xiao@ucsd.edu>
2022-10-20 15:28:14 -0700

    add config file from ignore

---
+++ configs/d4rl/transform_d4rl_dataset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python data/dataset_d4rl.py --task=pen-human-v0
+python data/dataset_d4rl.py --task=relocate-human-v0
+python data/dataset_d4rl.py --task=hammer-human-v0
+python data/dataset_d4rl.py --task=door-human-v0
