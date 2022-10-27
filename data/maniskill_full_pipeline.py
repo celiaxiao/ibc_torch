08824ee315190336d0eaed8d2595957bdf697e12
celiaxiao <c6xiao@ucsd.edu>
2022-10-27 12:23:56 -0700

    update maniskill pipeline for sequence dataset

--- data/maniskill_full_pipeline.py
+++ data/maniskill_full_pipeline.py
@@ -1,23 +1,16 @@
 '''
 Full pipeline to process maniskill demos in .h5 and .json to customized dataset.
 Argument specification: TODO
-<<<<<<< HEAD
-=======
 
 exmaple to transform h5:
 CUDA_VISIBLE_DEVICES=0 python data/maniskill_full_pipeline.py \
  --h5_path=data/softbody/Hang-v0/trajectory.none.pd_joint_delta_pos.h5 --json_path=data/softbody/Hang-v0/trajectory.none.pd_joint_delta_pos.json \
  --env_name=Hang-v0 --new_h5_path=data/softbody/Hang-v0/
->>>>>>> temp
 '''
 from absl import flags
 import sys
 import os
-<<<<<<< HEAD
-
-=======
 import warnings
->>>>>>> temp
 # from pyrl.utils.data import GDict
 import h5py
 import json
@@ -25,36 +18,20 @@ import torch
 import numpy as np
 from data.dataset_maniskill import *
 from environments.maniskill.maniskill_env import *
+import gym
 
 flags.DEFINE_string('h5_path', None, 'h5 demo path')
 flags.DEFINE_string('json_path', None, 'json demo path')
-<<<<<<< HEAD
-flags.DEFINE_string('env_name', None, 'env name')
-flags.DEFINE_enum('obs_mode', default='particles', enum_values=['particles'], help='')
-flags.DEFINE_enum('control_mode', default='pd_joint_delta_pos', enum_values=['pd_joint_delta_pos'], help='env control_mode')
-flags.DEFINE_string('raw_data_path', None, '')
-flags.DEFINE_string('dataset_path', None, '')
-=======
 flags.DEFINE_string('env_name', 'Hang-v0', 'env name')
-flags.DEFINE_enum('obs_mode', default='particles', enum_values=['particles'], help='')
+flags.DEFINE_enum('obs_mode', default='particles', enum_values=['particles', 'state'], help='')
 flags.DEFINE_enum('control_mode', default='pd_joint_delta_pos', enum_values=['pd_joint_delta_pos'], help='env control_mode')
 flags.DEFINE_string('raw_data_path', None, 'Path to save raw obs/act')
 flags.DEFINE_string('dataset_path', None, 'Path to save torch dataset')
 flags.DEFINE_string('new_h5_path', None, 'Path to save processed h5 file')
->>>>>>> temp
 
 FLAGS = flags.FLAGS
 FLAGS(sys.argv)
 
-<<<<<<< HEAD
-def convert_dataset(h5_path, json_path, target_env, raw_data_path, dataset_path, prefix):
-    h5_demo = h5py.File(h5_path, 'r')
-    # h5_demo = GDict.from_hdf5(h5_path)
-    json_data = json.load(open(json_path))
-
-    all_obs = []
-    all_actions = []
-=======
 def convert_dataset(h5_path, json_path, target_env, raw_data_path, dataset_path, new_h5_path, prefix):
     h5_demo = h5py.File(h5_path, 'r')
     # h5_demo = GDict.from_hdf5(h5_path)
@@ -64,16 +41,11 @@ def convert_dataset(h5_path, json_path, target_env, raw_data_path, dataset_path,
     all_actions = []
     all_rewards = []
     all_dones = []
->>>>>>> temp
 
     for episode in json_data['episodes']:
         episode_id = 'traj_' + str(episode['episode_id'])
         target_env.reset(seed=episode["episode_seed"])
-<<<<<<< HEAD
-
-=======
         print("starting episode", episode_id)
->>>>>>> temp
         for action in h5_demo[episode_id]['actions']:
             obs = target_env.get_obs()
 
@@ -85,24 +57,6 @@ def convert_dataset(h5_path, json_path, target_env, raw_data_path, dataset_path,
                 print('obs shape', obs.shape)
                 print('action shape', action.shape)
 
-<<<<<<< HEAD
-            target_env.step(action)
-
-        print(f'finished {episode_id}, success status {target_env.evaluate()}')
-
-    np.save(f'{raw_data_path}/{prefix}_observations.npy', all_obs)
-    np.save(f'{raw_data_path}/{prefix}_actions.npy', all_actions)
-
-    dataset = maniskill_dataset(all_obs, all_actions)
-    torch.save(dataset, f"{dataset_path}/{prefix}_dataset.pt")
-
-if __name__ == '__main__':
-
-    if not os.path.exists(FLAGS.raw_data_path):
-        os.makedirs(FLAGS.raw_data_path)
-    if not os.path.exists(FLAGS.dataset_path):
-        os.makedirs(FLAGS.dataset_path)
-=======
             _, rew, done, _ = target_env.step(action)
             all_rewards.append(rew)
             all_dones.append(done)
@@ -140,9 +94,9 @@ if __name__ == '__main__':
         os.makedirs(FLAGS.dataset_path)
     if FLAGS.new_h5_path and not os.path.exists(FLAGS.new_h5_path):
         os.makedirs(FLAGS.new_h5_path)
->>>>>>> temp
 
     # Currently only supporting particles obs mode
+    fn = None
     if FLAGS.env_name == 'Hang-v0':
         fn = HangEnvParticle
     elif FLAGS.env_name == 'Fill-v0':
@@ -150,12 +104,11 @@ if __name__ == '__main__':
     elif FLAGS.env_name == 'Excavate-v0':
         fn = ExcavateEnvParticle
 
-    target_env = fn(control_mode=FLAGS.control_mode, obs_mode=FLAGS.obs_mode)
+    if fn is not None:
+        target_env = fn(control_mode=FLAGS.control_mode, obs_mode=FLAGS.obs_mode)
+    else:
+        target_env = gym.make(FLAGS.env_name, control_mode=FLAGS.control_mode, obs_mode=FLAGS.obs_mode)
     prefix = f"{FLAGS.env_name}_{FLAGS.obs_mode}_{FLAGS.control_mode}"
     print(prefix)
 
-<<<<<<< HEAD
-    convert_dataset(h5_path=FLAGS.h5_path, json_path=FLAGS.json_path, target_env=target_env, raw_data_path=FLAGS.raw_data_path, dataset_path=FLAGS.dataset_path, prefix=prefix)
-=======
     convert_dataset(h5_path=FLAGS.h5_path, json_path=FLAGS.json_path, target_env=target_env, raw_data_path=FLAGS.raw_data_path, dataset_path=FLAGS.dataset_path, new_h5_path=FLAGS.new_h5_path, prefix=prefix)
->>>>>>> temp
