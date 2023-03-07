9c86dded8e8706dcb30972e92bc517e5b5920971
celiaxiao <c6xiao@ucsd.edu>
2023-03-07 15:39:42 -0800

    dynamically control compositionPoints success criteria

--- environments/maniskill/composition_env.py
+++ environments/maniskill/composition_env.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 class CompositionPoints(gym.Env):
     
-    def __init__(self, obs_mode=4, *args, **kwargs) -> None:
+    def __init__(self, obs_mode=4, control_mode=1e-2, *args, **kwargs) -> None:
         super().__init__()
         self.num_points = int(obs_mode)
+        self.atol = float(control_mode)
         self.observation_space = gym.spaces.Box(low=0, high=1, shape=(self.num_points*4,))
         self.action_space = gym.spaces.Box(low=-1, high=1, shape=(self.num_points*2,))
         self.curr = np.random.rand(self.num_points, 2) 
@@ -24,15 +25,15 @@ class CompositionPoints(gym.Env):
         self.curr += action.reshape(self.curr.shape)
         self.curr = np.clip(self.curr, 0, 1)
         reward = self.evaluate()
-        done = np.allclose(self.curr, self.end, atol=1e-3)
+        done = np.allclose(self.curr, self.end, atol=self.atol)
         observation = self.get_obs()
         return observation, reward, done, {}
 
     def reset(self, seed=None):
         if seed is not None:
             self.seed(seed)
-        self.curr = np.random.rand(self.curr.shape) 
-        self.end = np.random.rand(self.end.shape) 
+        self.curr = np.random.rand(self.num_points, 2) 
+        self.end = np.random.rand(self.num_points, 2) 
         observation = self.get_obs()
         return observation
     
@@ -40,13 +41,13 @@ class CompositionPoints(gym.Env):
         plt.clf()
         plt.scatter(self.end[:, 0], self.end[:, 1], color='red')
         plt.scatter(self.curr[:, 0], self.curr[:, 1])
-        
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+
         # Convert the plot to a numpy array
         fig = plt.gcf()
-        canvas = FigureCanvas(fig)
-        canvas.draw()
-        raw_data = canvas.buffer_rgba()
-        img = np.asarray(raw_data)
+        fig.canvas.draw()
+        img = np.array(fig.canvas.renderer.buffer_rgba())
         return img
 
     def close(self):
